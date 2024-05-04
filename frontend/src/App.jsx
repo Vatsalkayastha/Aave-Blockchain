fe7fad15d487decfb2944f20ddbf7a1328442941
Vatsalkayastha <vatsal.k2@ahduni.edu.in>
2024-05-04 18:58:58 +0530

    Wallet Connected frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css'
 import Wallet from './Components/Wallet'
 function App() {
   return (
     <>
-      <div className='font-thin'>Vatsal Blockchain Project</div> 
       <Wallet />     
     </>
   )
--- frontend/src/Components/Wallet.jsx
+++ frontend/src/Components/Wallet.jsx
@@ -1,39 +1,177 @@
-import React from 'react'
-import { useState } from 'react';
-// Import everything
+
+import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-// Import just a few select items
-import { BrowserProvider, parseUnits } from "ethers";
 
-// Import from a specific export
-import { HDNodeWallet } from "ethers/wallet";
-const Wallet = async () => {
-    let signer = null;
-    let provider;
-    
-    async function requestAccount() {
-        console.log('first')
-        if(window.ethereum==null){
-            console.log("MetaMask not installed; using read-only defaults")
-        } else {
-            const provider = new ethers.BrowserProvider(window.ethereum);
-            console.log(provider);
-            const balance = await provider.getBalance("ethers.eth")
-            console.log(formatEther(balance));
-            signer = await provider.getSigner();
-        }
+
+function Wallet() {
+  let signer;
+  let provider;
+;
+
+  const [addr, setAddr] = useState(""); //destination addr
+  const [amount, setAmount] = useState("");
+  const [balance, setBalance] = useState(0);
+  const [accountAddr, setAccountAddr] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [signerAddr, setSignerAddr] = useState(null);
+  const [txStatus, setTxStatus] = useState("not-initialized");
+
+  const connect = async () => {
+    try {
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+      // MetaMask requires requesting permission to connect users accounts
+      await provider.send("eth_requestAccounts", []);
+      // console.log(provider);
+      signer = provider.getSigner();
+      // console.log(signer);
+      setSignerAddr(signer);
+
+      const accAddr = await signer.getAddress();
+      setAccountAddr(accAddr);
+      setConnected(true);
+      await checkbalance(signer);
+      // await sendTransaction("0.02");
+    } catch (error) {
+      console.log(error);
     }
-   
+  };
+
+//   const tokenContract = new ethers.Contract(
+//     link_tokenAddress,
+//     linktoken_abi.result,
+//     signer
+//   );
+
+  const checkbalance = async (signer) => {
+    const balance = await signer.getBalance();
+    setBalance(balance);
+  };
+
+//   const sendTokensToContract = async (e, tokenAmount) => {
+//     e.preventDefault();
+//     try {
+//       const amountToSend = ethers.utils.parseUnits(tokenAmount.toString(), 18); // Convert token amount to Wei
+//       const tokenContractWithSigner = tokenContract.connect(signerAddr);
+//       const tx = await tokenContractWithSigner.transfer(
+//         contractAddress,
+//         amountToSend
+//       );
+//       await tx.wait();
+//       setTxStatus("completed");
+//       console.log("Tokens sent to contract successfully");
+//     } catch (error) {
+//       console.error("Sending tokens to contract failed:", error);
+//     }
+//   };
+
+//   const getStatus = (txStatus) => {
+//     switch (txStatus) {
+//       case "initialized":
+//         return "Pending";
+//       case "completed":
+//         return "Successfull";
+//       default:
+//         return "Not initialized";
+//         break;
+//     }
+//   };
+
+//   useEffect(() => {
+//     console.log("Balance: ", ethers.utils.formatEther(balance));
+//     // console.log(accountAddr);
+//     // console.log(txStatus);
+//   }, [balance]);
+
   return (
-    <div>
-        <button className='text-[16px] p-4 rounded-2xl bg-red-600 text-white'
-         onClick={requestAccount}>
-            Connect Wallet 
-        </button>
-        <h3>Wallet Address: oddd</h3>
+    <div className="flex h-[100vh] w-full">
+      <div>
+        <div className="text-center">
+          <button
+            className="text-right"
+            onClick={(e) => {
+              connect(e);
+            }}
+          >
+            {connected ? "Connected" : "Connect to Metamask"}
+          </button>
+        </div>
+
+        <div className="text-center mb-4">
+          <div className="mb-3">
+            <label htmlFor="accountAddr" className="form-label">
+              Connected Account
+            </label>
+            <input
+              id="accountAddr"
+              type="text"
+              value={accountAddr}
+              aria-label="Account address"
+              disabled
+              readOnly
+            />
+          </div>
+          <div>
+            <button>
+              Balance: {ethers.utils.formatUnits(balance, 18)}
+            </button>
+          </div>
+        </div>
+
+        {/* <div className="text-center border rounded p-4">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Destination Address
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                value={addr}
+                onChange={(e) => setAddr(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Amount
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputPassword1"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+          </form>
+          <Button
+            type="submit"
+            className="btn btn-danger mb-3"
+            variant="contained"
+            color="error"
+            onClick={(e) => {
+              sendTokensToContract(e, amount);
+              setAddr("");
+              setAmount("");
+              setTxStatus("initialized");
+            }}
+          >
+            Transact
+          </Button>
+          <div id="liveAlertPlaceholder"></div>
+          <button
+            type="button"
+            className="btn btn-primary mb-3"
+            id="liveAlertBtn"
+          >
+            status: {getStatus(txStatus)}
+          </button>
+        </div> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Wallet
+export default Wallet;
